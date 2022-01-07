b612953a6c15720582cdcd10504ef815d24cebce
Bryan Loh <lamofacade@gmail.com>
2022-01-07 10:45:42 +0800

    Add firebase and firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+
+const app = express();
+
+exports.app = functions.https.onRequest(app);
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
